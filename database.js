e8cb2cbebf7d92e81f71cbff1b0d8b6cfc7e9b08
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-06-19 19:23:18 -0700

    Add user register route

--- database.js
+++ database.js
@@ -1,36 +1,39 @@
-const pg = require('pg')
+// const pg = require('pg')
+const promise = require('bluebird')
+const options = {
+  promiseLib: promise
+}
+
+const pgp = require('pg-promise')(options)
 
 const dbName = 'vinyl'
 const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
-const client = new pg.Client(connectionString)
+const db = pgp(connectionString)
 
-client.connect()
+const getAlbums = () => { return db.any('SELECT * FROM albums') }
 
-// Query helper function
-const query = function(sql, variables, callback){
-  console.log('QUERY ->', sql.replace(/[\n\s]+/g, ' '), variables)
-
-  client.query(sql, variables, function(error, result){
-    if (error){
-      console.log('QUERY <- !!ERROR!!')
-      console.error(error)
-      callback(error)
-    }else{
-      console.log('QUERY <-', JSON.stringify(result.rows))
-      callback(error, result.rows)
-    }
-  })
+const getAlbumsByID = (albumID) => {
+  return db.one('SELECT * FROM albums WHERE id = $1', [albumID])
 }
 
-const getAlbums = function(callback) {
-  query("SELECT * FROM albums", [], callback)
+const createNewUser = (data) => {
+  const sql = 'INSERT INTO users(name, email, password) values($1, $2, $3) RETURNING id'
+  const variables = [
+    data.name,
+    data.email,
+    data.password
+  ]
+
+  return db.one(sql, variables)
 }
 
-const getAlbumsByID = function(albumID, callback) {
-  query("SELECT * FROM albums WHERE id = $1", [albumID], callback)
+const findUserByID = (userID) => {
+  return db.one('SELECT * FROM users WHERE id = $1', [userID])
 }
 
 module.exports = {
   getAlbums,
-  getAlbumsByID
+  getAlbumsByID,
+  createNewUser,
+  findUserByID
 }
--- routes/index.js
+++ routes/index.js
@@ -2,51 +2,67 @@ const express = require('express')
 const router = express.Router()
 const database = require('../database')
 
-router.get('/', (request, response) => {
-  database.getAlbums((error, albums) => {
-    if (error) {
-      response.status(500).render('error', { error: error })
-    } else {
-      response.render('index', { albums: albums, title: 'Home' })
-    }
+router.get('/', (req, res) => {
+  database.getAlbums()
+  .then((albums) => {
+    res.render('index', { albums: albums })
+  })
+  .catch(error => {
+    res.status(500).render('error', { error: error })
   })
 })
 
-router.get('/albums/:albumID', (request, response) => {
-  const albumID = request.params.albumID
+router.get('/albums/:albumID', (req, res) => {
+  const albumID = req.params.albumID
 
-  database.getAlbumsByID(albumID, (error, albums) => {
-    if (error) {
-      response.status(500).render('error', { error: error })
-    } else {
-      const album = albums[0]
-      response.render('album', { album: album })
-    }
-  })
+  database.getAlbumsByID(albumID)
+    .then((album) => {
+      res.render('album', { album: album })
+    })
+    .catch((error) => {
+      res.status(500).render('error', { error })
+    })
 })
 
-router.get('/users/register', (request, response) => {
-  response.render('register', { title: 'Sign up for Vinyl' })
+router.get('/users/register', (req, res) => {
+  res.render('register', { title: 'Sign up for Vinyl' })
 })
 
-router.get('/users/login', (request, response) => {
-  response.render('login', { title: 'Sign In' })
+router.get('/users/login', (req, res) => {
+  res.render('login', { title: 'Sign In' })
 })
 
-router.post('/users/register', (request, response) => {
-
-  request.checkBody('name', 'Name field is required').notEmpty()
-  request.checkBody('email', 'Email field is required').notEmpty()
-  request.checkBody('email', 'Email is not valid').isEmail()
-  request.checkBody('password', 'Password field is required').notEmpty()
-  request.checkBody('password2', 'Passwords do not match').equals(request.body.password)
-  const errors = request.validationErrors()
+router.post('/users/register', (req, res, next) => {
+  req.checkBody('name', 'Name field is required').notEmpty()
+  req.checkBody('email', 'Email field is required').notEmpty()
+  req.checkBody('email', 'Email is not valid').isEmail()
+  req.checkBody('password', 'Password field is required').notEmpty()
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password)
+  let errors = req.validationErrors()
+  let url;
 
   if (errors) {
-    response.render('register', { errors: errors, title: 'Sign up for Vinyl' })
+    res.render('register', { errors: errors, title: 'Sign up for Vinyl' })
   } else {
-    console.log('no errors')
+    database.createNewUser(req.body)
+      .then((user) => {
+        url = `/users/${user.id}`
+        res.redirect(url)
+      })
+      .catch((error) => {
+        res.status(500).render('error', { error })
+      })
   }
+})
+
+router.get('/users/:id', (req, res) => {
+  database.findUserByID(req.params.id)
+    .then((user) => {
+      res.render('profile', { user: user })
+    })
+    .catch((error) => {
+      res.status(500).render('error', { error })
+    })
 })
 
 module.exports = router
